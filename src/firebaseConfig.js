08e90430c17cb9f0c515e071b58c74cf0c1061b4
John Ivanov <ivanov.john04@gmail.com>
2025-12-10 19:14:58 -0800

    Fix firebaseConfig env usage in JS file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,14 @@
-// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5utIUSoqdLnFJtvArA1Un5PZc1jlK4GE",
-  authDomain: "soccersync-e2476.firebaseapp.com",
-  projectId: "soccersync-e2476",
-  storageBucket: "soccersync-e2476.firebasestorage.app",
-  messagingSenderId: "983551077392",
-  appId: "1:983551077392:web:e164637b755d918e9f2e95"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
